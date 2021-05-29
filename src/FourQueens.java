8ddcb3ab396e02bde2c07967c0cc3af070fdac7c
longnt912 <longchivasvolka@gmail.com>
2021-05-30 01:01:21 +0700

    Lmao Dark Burh

--- src/FourQueens.java
+++ src/FourQueens.java
@@ -1,65 +1,67 @@
 public class FourQueens {
-    static final int N = 4;
+        static final int N = 4;
 
-    static void InGiaiPhap(int board[][]) {
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++)
-                System.out.print(" " + board[i][j]
-                        + " ");
-            System.out.println();
-        }
-    }
-
-    // Kiem tra vi tri
-    static boolean ViTriAnToan(int board[][], int row, int col) {
-        int i, j;
-        for (i = 0; i < col; i++)
-            if (board[row][i] == 1)
-                return false;
-
-        for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
-            if (board[i][j] == 1)
-                return false;
-
-        for (i = row, j = col; j >= 0 && i < N; i++, j--)
-            if (board[i][j] == 1)
-                return false;
-
-        return true;
-    }
-
-    // ham dieu kien
-    public static boolean Try(int board[][], int col) {
-        if (col >= N)
-            return true;
-
-        for (int i = 0; i < N; i++) {
-            //Dat vao vi tri an toan
-            if (ViTriAnToan((board), i, col)) {
-                board[i][col] = 1;
-
-                if (Try(board, col + 1))
-                    return true;
-
-
-                board[i][col] = 0;
+        // print the final solution matrix
+        static void printSolution(int board[][]) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++)
+                    System.out.print(" " + board[i][j]
+                            + " ");
+                System.out.println();
             }
         }
-        return false;
-    }
 
-    // Tao ban co 4x4
-    public static void main(String args[]) {
-        int board[][] = {{0, 0, 0, 0},
-                {0, 0, 0, 0},
-                {0, 0, 0, 0},
-                {0, 0, 0, 0}};
+        // function to check whether the position is safe or not
+        static boolean isSafe(int board[][], int row, int col) {
+            int i, j;
+            for (i = 0; i < col; i++)
+                if (board[row][i] == 1)
+                    return false;
 
-        if (!Try(board, 0)) {
-            System.out.print("Khong co phuong phap");
-            return;
+
+            for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
+                if (board[i][j] == 1)
+                    return false;
+
+            for (i = row, j = col; j >= 0 && i < N; i++, j--)
+                if (board[i][j] == 1)
+                    return false;
+
+            return true;
         }
-        InGiaiPhap(board);
 
+        // The function that solves the problem using backtracking
+        public static boolean solveNQueen(int board[][], int col) {
+            if (col >= N)
+                return true;
+
+            for (int i = 0; i < N; i++) {
+                //if it is safe to place the queen at position i,col -> place it
+                if (isSafe(board, i, col)) {
+                    board[i][col] = 1;
+
+                    if (solveNQueen(board, col + 1))
+                        return true;
+
+                    //backtrack if the above condition is false
+                    board[i][col] = 0;
+                }
+            }
+            return false;
+        }
+
+        public static void main(String args[]) {
+            int board[][] = {{0, 0, 0, 0},
+                    {0, 0, 0, 0},
+                    {0, 0, 0, 0},
+                    {0, 0, 0, 0}};
+
+            if (!solveNQueen(board, 0)) {
+                System.out.print("Solution does not exist");
+                return;
+            }
+
+            printSolution(board);
+
+        }
     }
-}
