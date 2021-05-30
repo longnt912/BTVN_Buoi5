9e08e8651678e7f99d9073cedb53f9adcf193e30
longnt912 <longchivasvolka@gmail.com>
2021-05-30 20:43:04 +0700

    Upload

--- src/Rabbits.java
+++ src/Rabbits.java
@@ -6,13 +6,13 @@ public class Rabbits {
         System.out.print("Nhap so thang: ");
         int x = input.nextInt();
         for (int i = 1; i <= x; i++) {
-            System.out.println(" " + i + " Tong so tho sinh ra tren thang " + f(i));
+            System.out.println(" Thang " + i + " Tong so cap tho sinh ra tren thang " + f(i) + " cap");
         }
 
     }
 
     public static int f(int x) {
-        if (x <= 1) {
+        if (x ==1 || x==2) {
             return 1;
         } else {
             return f(x - 1) + f(x - 2);
